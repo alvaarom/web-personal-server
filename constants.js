a939efc696066af96e6f23337bd14f50eecc3b78
Alvaro Malvestiti <alvaromalvestiti@gmail.com>
2023-07-12 12:22:47 -0300

    [ADD] jwt

--- constants.js
+++ constants.js
@@ -5,10 +5,13 @@ const DB_HOST = "personal-web.yt13ijz.mongodb.net";
 const API_VERSION = "v1";
 const IP_SERVER = "localhost";
 
+const JWT_SECRET_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+
 module.exports = {
   DB_USER,
   DB_PASSWORD,
   DB_HOST,
   API_VERSION,
   IP_SERVER,
+  JWT_SECRET_KEY,
 };
